7a67ef4b4426ce881eb687ddf23190cb65fbb663
helmesjo <helmesjo@gmail.com>
2026-08-06 01:55:48 +0200

    bdep new --package --lang c++,cpp --type lib,split,subdir=bgfx,no-subdir-source,third-party libbgfx

---
+++ libbgfx/src/bgfx.cpp
@@ -0,0 +1,17 @@
+#include <bgfx/bgfx.hpp>
+
+#include <ostream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace bgfx
+{
+  void say_hello (ostream& o, const string& n)
+  {
+    if (n.empty ())
+      throw invalid_argument ("empty name");
+
+    o << "Hello, " << n << '!' << endl;
+  }
+}
